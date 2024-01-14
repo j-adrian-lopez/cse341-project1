eeca6af3434fdea2a3d25a10a3c4f5cb1ebb38f9
j-adrian-lopez <adrj156@byui.edu>
2024-01-13 22:27:05 -0300

    correct users route

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,6 @@ const router = require('express').Router();
 
 router.get('/', (req, res) => (res.send('Hello World')));
 
-router.use('/users', require('../controllers/users'));
-
-router.get('/', user)
+router.use('/users', require('./users'));
 
 module.exports = router;
