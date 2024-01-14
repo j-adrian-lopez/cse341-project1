b5ae6bf6f38330f93022fe1c91f4a55531acc9bd
j-adrian-lopez <adrj156@byui.edu>
2024-01-13 21:21:17 -0300

    Finished connection to database

--- routes/index.js
+++ routes/index.js
@@ -2,4 +2,8 @@ const router = require('express').Router();
 
 router.get('/', (req, res) => (res.send('Hello World')));
 
+router.use('/users', require('../controllers/users'));
+
+router.get('/', user)
+
 module.exports = router;
--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const app = express();
-// const dotenv = require("dotenv").config();
+
+const mongodb = require('./data/database')
 
 const port = process.env.PORT || 3000;
 
 app.use('/', require('./routes'));
 
-app.listen(port, () => (console.log(`Running on port ${port}`)));
+mongodb.initDb((err) => {
+    if(err) {
+        console.log(err);
+    } else {
+        app.listen(port, () => (console.log(`Database is listening and node running on port ${port}`)))
+    }
+})
